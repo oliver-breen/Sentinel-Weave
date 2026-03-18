8e3edc89a83a27a07293637a95318f108074c3a8
Oliver Breen <oliver.breen000@gmail.com>
2026-03-17 21:13:47 -0600

    claen up

--- install-dev-deps.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-# Install root dev dependencies
-python -m pip install -r requirements-dev.txt
-
-# Install liboqs-python dev extras
-python -m pip install -e .\liboqs-python[dev]
-
-Write-Host "Dev dependencies installed."
